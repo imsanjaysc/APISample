3d634def3adf509c139bef3d348a3ea7b16cb5bc
Sanjay Singh Chouhan <sschouhan4@gmail.com>
2019-01-30 22:42:26 +0530

    Add files via upload

---
+++ BAL/Encrypt.cs
@@ -0,0 +1,27 @@
+﻿using CCA.Util;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAL
+{
+    public class Encrypt
+    {
+        CCACrypto ccaCrypto = new CCACrypto();
+        public string EncryptStr(string data)
+        {
+            string key = ConfigurationManager.AppSettings["EncryKey"];
+            return ccaCrypto.Encrypt(data, key);
+
+        }
+        public string DecryptStr(string data)
+        {
+            string key = ConfigurationManager.AppSettings["EncryKey"];
+            return ccaCrypto.Decrypt(data, key);
+
+        }
+    }
+}
---
+++ BAL/OTP.cs
@@ -0,0 +1,46 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAL
+{
+    public class OTP
+    {
+        private Random _random = new Random();
+        SendSMSEmail sms = new SendSMSEmail();
+        private string GenerateRandomNo()
+        {
+            return _random.Next(0, 9999).ToString("D4");
+        }
+
+        public string SendOTP(string Mobileno)
+        {
+            string OTP = GenerateRandomNo();
+            sms.SendSMS(Mobileno, "Your OTP for FIXYBEE is " + OTP + "\nOTP valid for 15 minutes.");
+            return OTP;
+        }
+
+        public string SendRequestDetailsToCustomer(string Mobileno, string servicereqid)
+        {
+            sms.SendSMS(Mobileno, "Your request has been submitted, we will get back to you shortly. your Booking ID is " +servicereqid+ ". \nThanks for choosing FIXYBEE.");
+            return "Success";
+        }
+
+        public string SendRequestDetailsToAdmin(string Mobileno, string Name, string CustMobileNo, string Email, string Date, string Address, string servicereqid)
+        {
+            string CustEmail = Email != "" ? " Email- " +Email + "," : "";
+            sms.SendSMS(Mobileno, "Hi Fixybee, New Enquiry:\nName: " +Name+ "," +CustEmail+ " Mobile: "+ CustMobileNo + " for Date: "+Date+ ", Address: "+Address);
+            return "Success";
+        }
+        public string SendPassword(string Mobileno, string Password)
+        {
+            //string OTP = GenerateRandomNo();
+            sms.SendSMS(Mobileno, "Your Password for FIXYBEE is " + Password);
+            return "Success";
+        }
+
+    }
+}
